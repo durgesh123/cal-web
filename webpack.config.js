56ca8a6c4b76a4c0e37d16944de869aa6235da20
mohtasaurabh21 <tripathi.ror@gmail.com>
2019-11-13 00:38:09 +0530

    Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    entry: './src/index.jsx',
     resolve: {
         extensions: ['.js', '.jsx']
     },
